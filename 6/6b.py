299fe55655e982a9d4b465460cdc5d718ccaa965
peter.gergel <peter.gergel@segron.com>
2020-12-06 07:46:40 +0100

    6

--- 6/6b.py
+++ 6/6b.py
@@ -4,7 +4,7 @@ groups = open("input.txt").read().split("\n\n")
 count = 0
 for group in groups:
     yes_answers = set(ascii_lowercase)
-    for answers in group.split("\n"):
+    for answers in group.split():
         answers = set(answers)
         yes_answers = yes_answers.intersection(answers)
     count += len(yes_answers)
