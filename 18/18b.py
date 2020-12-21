e5066971cb212b2c39ecbcf411c637bfba1db7a0
peter.gergel <peter.gergel@segron.com>
2020-12-21 09:35:47 +0100

    18

---
+++ 18/18b.py
@@ -0,0 +1,48 @@
+import re
+data = open("input.txt").read().split("\n")
+
+
+def evaluate_simple(expression):
+    if "+" in expression and "*" in expression:
+        match = re.findall("\d+ \+ \d+", expression)[0]
+        expression = expression.replace(match, f"({match})")
+        return evaluate(expression)
+
+    result = 0
+    op = None
+
+    for idx, char in enumerate(expression.split()):
+        if char.isdigit():
+            char = int(char)
+            if op == "+":
+                result += char
+            elif op == "*":
+                result *= char
+            else:
+                result = char
+        else:
+            op = char
+
+    return result
+
+def evaluate(expression):
+    while True:
+        left_bracket_idx = -1
+        right_bracket_idx = -1
+        for idx in range(len(expression)):
+
+            if expression[idx] == ")":
+                right_bracket_idx = idx
+                simpl_epxression = expression[left_bracket_idx+1:right_bracket_idx]
+                result = evaluate_simple(simpl_epxression)
+                expression = expression.replace(f"({simpl_epxression})", str(result))
+                break
+
+            if expression[idx] == "(":
+                left_bracket_idx = idx
+
+        if left_bracket_idx == -1 and right_bracket_idx == -1:
+            return evaluate_simple(expression)
+
+print(sum([evaluate(line) for line in data]))
+
